8d512b2852e9a03ab4aae011125af5720bc56067
Safi649 <muhammadabbassafi332@gmail.com>
2025-07-24 21:52:15 +0500

    Create dashboard.js

---
+++ pages/admin/dashboard.js
@@ -0,0 +1,8 @@
+// pages/admin/dashboard.js
+export default function Dashboard() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white text-center">
+      <h1 className="text-4xl font-bold">Welcome to SafiBuilds Dashboard 🚀</h1>
+    </div>
+  );
+}
