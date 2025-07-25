5c877619638150f60be96d2565b32c3b2418b81a
Safi649 <muhammadabbassafi332@gmail.com>
2025-07-25 05:46:41 +0500

    Update dashboard.js

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,7 +1,7 @@
 // 📁 pages/admin/dashboard.js
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { auth } from '@/firebase/config';
+import { auth } from '../../firebase/config';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { toast } from 'react-hot-toast';
 
